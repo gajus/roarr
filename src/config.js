f4020560722888b8da2c4b0ce531550352c8c97e
Gajus Kuizinas <gajus@gajus.com>
2018-05-06 13:57:46 +0100

    fix: corrent validation logic

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ import parseBoolean from 'boolean';
 const ROARR_LOG = parseBoolean(process.env.ROARR_LOG) === true;
 const ROARR_STREAM = (process.env.ROARR_STREAM || 'STDOUT').toUpperCase();
 
-if (ROARR_STREAM !== 'STDOUT' || ROARR_STREAM !== 'STDERR') {
+if (ROARR_STREAM !== 'STDOUT' && ROARR_STREAM !== 'STDERR') {
   throw new Error('Unexpected ROARR_STREAM value.');
 }
 
