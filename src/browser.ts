2163becd81bb895944dd3d33ecbe85fbaccb2a14
Gajus Kuizinas <gajus@gajus.com>
2021-03-24 19:53:56 -0700

    fix: add missing browser.ts file

---
+++ src/browser.ts
@@ -0,0 +1,35 @@
+import createGlobalThis from 'globalthis';
+import {
+  createLogger,
+  createRoarrInitialGlobalState,
+} from './factories';
+import type {
+  RoarrGlobalState,
+} from './types';
+
+const globalThis = createGlobalThis();
+
+const ROARR = globalThis.ROARR = createRoarrInitialGlobalState(globalThis.ROARR as RoarrGlobalState || {});
+
+const logFactory = createLogger;
+
+export type {
+  Logger,
+  Message,
+  TranslateMessageFunction,
+  MessageEventHandler,
+} from './types';
+
+export {
+  ROARR,
+};
+
+export default logFactory((message) => {
+  if (ROARR.write) {
+    // Stringify message as soon as it is received to prevent
+    // properties of the context from being modified by reference.
+    const body = JSON.stringify(message);
+
+    ROARR.write(body);
+  }
+});
