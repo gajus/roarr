53192e12952088711234d25e8d6c6f4b51b64446
Gajus Kuizinas <gajus@gajus.com>
2021-09-12 16:55:04 -0500

    test: ensure that ROARR.write can be overriten in Node.js (fixes #52)

---
+++ test/roarr/integration.ts
@@ -0,0 +1,23 @@
+import test, {
+  beforeEach,
+} from 'ava';
+import sinon from 'sinon';
+import {
+  Roarr,
+  ROARR,
+} from '../../src/Roarr';
+
+test('ROARR.write overrides message handler', (t) => {
+  const messages = [];
+
+  const stub = sinon.stub(ROARR, 'write');
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  stub.callsFake(() => {});
+
+  Roarr.info('foo');
+
+  t.is(stub.firstCall.args.length, 1);
+
+  t.regex(stub.firstCall.args[0], /{"context":{"logLevel":30},"message":"foo","sequence":0,"time":\d+,"version":"\d\.\d\.\d"}/);
+});
