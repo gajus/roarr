03288677b67d106e8230a33170f089936eefdd93
Gajus Kuizinas <gajus@gajus.com>
2021-09-12 22:49:05 -0500

    chore: ignore ts error in Travis

--- test/roarr/integration.ts
+++ test/roarr/integration.ts
@@ -13,6 +13,10 @@ test('ROARR.write overrides message handler', (t) => {
 
   Roarr.info('foo');
 
+  // Travis is throwing error that cannot be replicated locally, e.g.
+  // https://app.travis-ci.com/github/gajus/roarr/builds/237479168
+  // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
+  // @ts-ignore
   t.is(stub.firstCall.args.length, 1);
 
   t.regex(stub.firstCall.args[0], /{"context":{"logLevel":30},"message":"foo","sequence":0,"time":\d+,"version":"\d\.\d\.\d"}/);
