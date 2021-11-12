d56e0238621574f8ab47674b00bf4c5465c60a93
Gajus Kuizinas <gajus@gajus.com>
2021-11-12 09:10:12 -0600

    feat: add getLogLevelName

---
+++ src/getLogLevelName.ts
@@ -0,0 +1,27 @@
+import type {
+  LogLevelName,
+} from './types';
+
+export const getLogLevelName = (numericLogLevel: number): LogLevelName => {
+  if (numericLogLevel <= 10) {
+    return 'trace';
+  }
+
+  if (numericLogLevel <= 20) {
+    return 'debug';
+  }
+
+  if (numericLogLevel <= 30) {
+    return 'info';
+  }
+
+  if (numericLogLevel <= 40) {
+    return 'warn';
+  }
+
+  if (numericLogLevel <= 50) {
+    return 'error';
+  }
+
+  return 'fatal';
+};
