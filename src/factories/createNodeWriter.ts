37dbe58b5cc4273e7ddf8982035b7a860589b326
Gajus Kuizinas <gajus@gajus.com>
2023-03-22 04:39:39 -0600

    feat: ignore EPIPE

--- src/factories/createNodeWriter.ts
+++ src/factories/createNodeWriter.ts
@@ -13,5 +13,13 @@ export const createNodeWriter = (): LogWriter => {
   const stream =
     targetStream.toUpperCase() === 'STDOUT' ? process.stdout : process.stderr;
 
+  stream.on('error', (error) => {
+    if (error.code === 'EPIPE') {
+      return;
+    }
+
+    throw error;
+  });
+
   return createBlockingWriter(stream);
 };
