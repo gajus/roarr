41d23fb891eac5233b6f4949cb151d790c3794ba
Gajus Kuizinas <gajus@gajus.com>
2023-09-27 15:57:14 -0400

    test: narrow down the issue

--- src/factories/createNodeWriter.ts
+++ src/factories/createNodeWriter.ts
@@ -1,25 +1,8 @@
 import { type LogWriter } from '../types';
 
-const createBlockingWriter = (stream: NodeJS.WritableStream): LogWriter => {
-  return (message: string) => {
-    stream.write(message + '\n');
-  };
-};
+// This is causing memory leak warning.
+process.stdout.on('error', () => {});
 
 export const createNodeWriter = (): LogWriter => {
-  // eslint-disable-next-line node/no-process-env
-  const targetStream = (process.env.ROARR_STREAM ?? 'STDOUT').toUpperCase();
-
-  const stream =
-    targetStream.toUpperCase() === 'STDOUT' ? process.stdout : process.stderr;
-
-  stream.on('error', (error) => {
-    if (error.code === 'EPIPE') {
-      return;
-    }
-
-    throw error;
-  });
-
-  return createBlockingWriter(stream);
+  return () => {};
 };
