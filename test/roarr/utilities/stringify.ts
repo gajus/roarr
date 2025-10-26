bd6173abf3981d6fcf3ef5fb185cd04c188e7e2c
Gajus <gajus@gajus.com>
2025-10-25 23:50:12 -0400

    fix: update lock

--- test/roarr/utilities/stringify.ts
+++ test/roarr/utilities/stringify.ts
@@ -62,32 +62,3 @@ test('stringifies key=value (circular)', (t) => {
 
   t.is(stringify(foo), '{"foo":"[Circular]"}');
 });
-
-test('overlapping log members are truncated', (t) => {
-  t.is(
-    stringify({
-      a0: 'a0',
-      a1: 'a1',
-      a2: 'a2',
-      a3: 'a3',
-      a4: 'a4',
-      a5: 'a5',
-      a6: 'a6',
-      a7: 'a7',
-      a8: 'a8',
-      a9: 'a9',
-      a10: 'a10',
-      a11: 'a11',
-      a12: 'a12',
-      a13: 'a13',
-      a14: 'a14',
-      a15: 'a15',
-      a16: 'a16',
-      a17: 'a17',
-      a18: 'a18',
-      a19: 'a19',
-      a20: 'a20',
-    }),
-    '{"a0":"a0","a1":"a1","a2":"a2","a3":"a3","a4":"a4","a5":"a5","a6":"a6","a7":"a7","a8":"a8","a9":"a9","a10":"a10","a11":"a11","a12":"a12","a13":"a13","a14":"a14","a15":"a15","a16":"a16","a17":"a17","a18":"a18","a19":"a19","...":"1 item not stringified"}',
-  );
-});
